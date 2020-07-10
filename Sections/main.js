33faabf940f3ede7ef9883c44ef76b25c69aef2e
Kieran Farrer <57142909+KieranFarrerDev@users.noreply.github.com>
2020-07-10 14:37:45 +0100

    Create main.js

---
+++ Sections/main.js
@@ -0,0 +1,38 @@
+import React, {Component} from "react";
+import LargeCartoonComponent from '../components/LargeCartoonComponent';
+import AboutMeComponent from '../components/AboutMeComponent';
+import SmallCartoonComponent from '../components/SmallCartoonComponent';
+import ProjectsLinkComponent from '../components/ProjectsLinkComponent';
+
+class Main1 extends Component {
+  state = {};
+  render(){
+    return (
+
+      <div className = "main-section" id = "main">
+
+<div className = "cartoon-section-lrg">
+    <LargeCartoonComponent1/>
+</div>
+
+<div className = "cartoon-section-sml">
+    <SmallCartoonComponent/>
+</div>
+
+<div id ="container-main-text">
+  <div id="main-text">
+  <AboutMeComponent/>
+
+    <div id="main-text-links">
+    <ProjectsLinkComponent/>
+    </div>
+
+</div>
+       </div>
+       </div>
+  );
+
+  }
+}
+
+export default Main1;
